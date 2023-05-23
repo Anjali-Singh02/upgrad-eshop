63ef6e0b1a277686e5ac075475f074dfad261b3e
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-24 00:13:18 +0530

    provide route to Home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import Home from './pages/Home';
 
 const App = () => {
 	return (
 		<>
 			<div>
 				<Routes>
-					<Route path="/" element={<SignupForm />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/signup" element={<SignupForm />} />
 					<Route path="/login" element={<LoginForm />} />
 				</Routes>
 			</div>
