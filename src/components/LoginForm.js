3507e367a798ddfc6c78884b95fbef39f040de0e
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-23 14:29:52 +0530

    applied validation and authentication

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,52 @@
 import './form.css';
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { validateLogin } from '../utils/helper';
+import { BackendContext } from '../context/context';
+import jwt_decode from 'jwt-decode';
 
 const LoginForm = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [allUsers, setAllUsers] = useState('');
+	const { baseUrl } = useContext(BackendContext);
+	const [user, setUser] = useState({
+		email: '',
+		password: '',
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setUser((ref) => ({ ...ref, [name]: value }));
+		//{email:'',password:''}
+	};
 
 	const submitForm = (e) => {
 		e.preventDefault();
-		if (email && password) {
-			const newArray = { email, password };
+		const isValid = validateLogin(user);
 
-			console.log(newArray);
-
-			setAllUsers([...allUsers, newArray]);
-			setEmail('');
-			setPassword('');
-		} else {
-			alert('Please fill all the input fields');
+		if (!isValid) {
+			console.log("Email password can't be empty");
+			return;
 		}
+
+		fetch(`${baseUrl}api/v1/auth`, {
+			headers: { 'Content-Type': 'application/json' },
+			method: 'POST',
+			body: JSON.stringify(user),
+		})
+			.then((response) => {
+				const token = response.headers.get('X-Auth-Token');
+				console.log(token);
+				const decode = jwt_decode(token);
+				console.log(decode);
+				sessionStorage.setItem('uuid', decode._id);
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				//set Cookie
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 	return (
 		<Fragment>
@@ -34,16 +61,16 @@ const LoginForm = () => {
 						placeholder="Email"
 						id="email"
 						name="email"
-						value={email}
-						onChange={(event) => setEmail(event.target.value)}
+						value={user.email}
+						onChange={handleChange}
 					/>
 					<input
 						type="password"
 						placeholder="Password"
 						id="password"
 						name="password"
-						value={password}
-						onChange={(event) => setPassword(event.target.value)}
+						value={user.password}
+						onChange={handleChange}
 					/>
 					<p>Forgot password?</p>
 					<button type="submit" className="btn btnSignIn">
