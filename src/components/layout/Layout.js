086a76435b8e62a9f9982c86cb710a7cd992ab56
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-25 22:20:36 +0530

    wrapped all the components inside SessionContext

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,38 @@
 import Navbar from '../Navbar';
-
+import { SessionContext } from '../../context/context';
+import { useEffect, useState } from 'react';
 const Layout = ({ children }) => {
+	const [state, setState] = useState({
+		username: '',
+		email: '',
+	});
+	useEffect(() => {
+		setState({
+			username: sessionStorage.getItem('name') || 'Guest',
+			email: sessionStorage.getItem('email') || '',
+		});
+	}, []);
 	return (
 		<div>
-			<Navbar />
-			<div style={{ textAlign: 'center', width: '100%', margin: 'auto' }}>
-				{children}
-			</div>
+			<SessionContext.Provider
+				value={{
+					username: state.username,
+					email: state.email,
+					setState: setState,
+				}}
+			>
+				<Navbar />
+
+				<div
+					style={{
+						textAlign: 'center',
+						width: '100%',
+						margin: 'auto',
+					}}
+				>
+					{children}
+				</div>
+			</SessionContext.Provider>
 		</div>
 	);
 };
