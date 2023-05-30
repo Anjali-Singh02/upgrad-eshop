41ed415802b1f762f4eb90153fff602fd0a66bd3
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-30 17:02:26 +0530

    removed items cart and placed into separate file and used custom scrollbar component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './cart.css';
 import { BsArrowLeft } from 'react-icons/bs';
 import { FaShoppingCart, FaTrash } from 'react-icons/fa';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import { IconContext } from 'react-icons/lib';
 import { Link } from 'react-router-dom';
-import { Button, TextField } from '@mui/material';
+import Scrollbars from 'react-custom-scrollbars-2';
+import Items from '../components/Items';
 
 const Cart = () => {
 	return (
@@ -41,162 +41,9 @@ const Cart = () => {
 				</p>
 				<div className="cart-items">
 					<div className="cart-items-container">
-						<div className="items-info">
-							<div className="product-img">
-								<img
-									src="https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-m20-m205f-1.jpg"
-									alt="image"
-								/>
-							</div>
-
-							<div className="title">
-								<h2>Samsung</h2>
-								<p>Red color</p>
-							</div>
-							<div className="add-quantity">
-								<Button className="minus-btn">
-									<AiOutlineMinus />
-								</Button>
-								<input
-									type="text"
-									className="textField"
-									placeholder="3"
-								/>
-								<Button className="plus-btn">
-									<AiOutlinePlus />
-								</Button>
-							</div>
-							<div className="price">
-								<h3>2000Rs</h3>
-							</div>
-							<div className="remove-item">
-								<Button>
-									<IconContext.Provider
-										value={{ color: 'red', size: '1.3em' }}
-									>
-										<FaTrash />
-									</IconContext.Provider>
-								</Button>
-							</div>
-						</div>
-						<hr />
-						<div className="items-info">
-							<div className="product-img">
-								<img
-									src="https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-m20-m205f-1.jpg"
-									alt="image"
-								/>
-							</div>
-
-							<div className="title">
-								<h2>Samsung</h2>
-								<p>Red color</p>
-							</div>
-							<div className="add-quantity">
-								<Button className="minus-btn">
-									<AiOutlineMinus />
-								</Button>
-								<input
-									type="text"
-									className="textField"
-									placeholder="3"
-								/>
-								<Button className="plus-btn">
-									<AiOutlinePlus />
-								</Button>
-							</div>
-							<div className="price">
-								<h3>2000Rs</h3>
-							</div>
-							<div className="remove-item">
-								<Button>
-									<IconContext.Provider
-										value={{ color: 'red', size: '1.3em' }}
-									>
-										<FaTrash />
-									</IconContext.Provider>
-								</Button>
-							</div>
-						</div>
-						<hr />
-						<div className="items-info">
-							<div className="product-img">
-								<img
-									src="https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-m20-m205f-1.jpg"
-									alt="image"
-								/>
-							</div>
-
-							<div className="title">
-								<h2>Samsung</h2>
-								<p>Red color</p>
-							</div>
-							<div className="add-quantity">
-								<Button className="minus-btn">
-									<AiOutlineMinus />
-								</Button>
-								<input
-									type="text"
-									className="textField"
-									placeholder="3"
-								/>
-								<Button className="plus-btn">
-									<AiOutlinePlus />
-								</Button>
-							</div>
-							<div className="price">
-								<h3>2000Rs</h3>
-							</div>
-							<div className="remove-item">
-								<Button>
-									<IconContext.Provider
-										value={{ color: 'red', size: '1.3em' }}
-									>
-										<FaTrash />
-									</IconContext.Provider>
-								</Button>
-							</div>
-						</div>
-						<hr />
-						<div className="items-info">
-							<div className="product-img">
-								<img
-									src="https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-m20-m205f-1.jpg"
-									alt="image"
-								/>
-							</div>
-
-							<div className="title">
-								<h2>Samsung</h2>
-								<p>Red color</p>
-							</div>
-							<div className="add-quantity">
-								<Button className="minus-btn">
-									<AiOutlineMinus />
-								</Button>
-								<input
-									type="text"
-									className="textField"
-									placeholder="3"
-								/>
-								<Button className="plus-btn">
-									<AiOutlinePlus />
-								</Button>
-							</div>
-							<div className="price">
-								<h3>2000Rs</h3>
-							</div>
-							<div className="remove-item">
-								<Button>
-									<IconContext.Provider
-										value={{ color: 'red', size: '1.3em' }}
-									>
-										<FaTrash />
-									</IconContext.Provider>
-								</Button>
-							</div>
-						</div>
-						<hr />
+						<Scrollbars>
+							<Items />
+						</Scrollbars>
 					</div>
 				</div>
 			</section>
